70ba79f5ea32bb456d8cb7f226d16b2a6b30326f
forestrr <afivmd78@gmail.com>
2021-08-15 03:08:14 +0400

    the date and number of guests added date have been formated dd mmmm yy format using the format module from ddate fns and debugged and made into a ranges and displyed dyanmicly

--- pages/search.js
+++ pages/search.js
@@ -1,19 +1,21 @@
-import { useRouter } from "next/router";
+import { useRouter } from "next/dist/client/router";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-
+import {format} from "date-fns"
 function search() {
     const router=useRouter()
     console.log(router.query)
-    const {location , startDate ,endDate,noOfGuests }=router.query;
-    console.log(startDate)
+    const {location , startDate ,endDate,noOfGuest }=router.query;
 
+    const formattedStartDate= format(new Date(startDate),"dd MMMM yy") 
+    const formattedEndDate= format(new Date(endDate),"dd MMMM yy")     
+    const ranges=`${formattedStartDate}-${formattedEndDate}`
     return (
         <div className ="">
             <Header/>
             <main className="flex">
                 <section className='flex-grow pt-14 px-16'>
-                    <p className='text-xs'>300 +stays for {noOfGuests} guests </p>
+                    <h3 className='text-xs'>300 stays from {ranges} for {noOfGuest} guests </h3>
                     <h1 className="text=3xl font-semibold mt-2 mb-6">stays in {location}</h1>
                     <div className='hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap'>
                         <p className='button'>Cancellation flexiblity</p>
